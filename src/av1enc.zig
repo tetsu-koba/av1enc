84ad2942f96ed601fef43eb8cc9552cac8e2e0ae
Tetsuyuki Kobayashi <tetu.koba@gmail.com>
2023-05-08 22:49:11 +0900

    Initial commit

---
+++ src/av1enc.zig
@@ -0,0 +1,131 @@
+const std = @import("std");
+const log = std.log;
+const rav1e = @cImport(@cInclude("rav1e.h"));
+
+pub const AV1Enc = struct {
+    width: u32,
+    height: u32,
+    framerate: u32,
+    time_scale: u32,
+    bitrate: u32,
+    keyframe_interval: u32,
+    config: *rav1e.RaConfig,
+    encoder: *rav1e.RaContext,
+
+    const Self = @This();
+
+    pub fn init(
+        width: u32,
+        height: u32,
+        framerate: u32,
+        time_scale: u32,
+        bitrate: u32,
+        keyframe_interval: u32,
+    ) !AV1Enc {
+        var self = AV1Enc{
+            .width = width,
+            .height = height,
+            .framerate = framerate,
+            .time_scale = time_scale,
+            .bitrate = bitrate,
+            .keyframe_interval = keyframe_interval,
+            .config = undefined,
+            .encoder = undefined,
+        };
+        var config: ?*rav1e.RaConfig = rav1e.rav1e_config_default();
+        if (config == null) {
+            log.err("Failed to create config", .{});
+            return error.FailedToCreateConfig;
+        }
+        self.config = config.?;
+
+        _ = rav1e.rav1e_config_parse_int(config, "width", @intCast(c_int, width));
+        _ = rav1e.rav1e_config_parse_int(config, "height", @intCast(c_int, height));
+        _ = rav1e.rav1e_config_parse_int(config, "key_frame_interval", @intCast(c_int, keyframe_interval));
+        //_ = rav1e.rav1e_config_parse_int(config, "bitrate", bitrate);
+        _ = rav1e.rav1e_config_parse_int(config, "speed", 9);
+        if (0 != rav1e.rav1e_config_parse(config, "low_latency", "true")) {
+            log.err("Failed to config low_latency", .{});
+        }
+
+        var encoder: ?*rav1e.RaContext = rav1e.rav1e_context_new(config);
+        if (encoder == null) {
+            log.err("Failed to create encoder", .{});
+            return error.FailedToCreateEncoder;
+        }
+        self.encoder = encoder.?;
+        return self;
+    }
+
+    pub fn deinit(self: *Self) void {
+        rav1e.rav1e_context_unref(self.encoder);
+        rav1e.rav1e_config_unref(self.config);
+    }
+
+    pub fn sendFrame(self: *Self, frame_data: []const u8, cb: *const fn([]const u8)void) !void {
+        const frame = rav1e.rav1e_frame_new(self.encoder);
+        if (frame == null) {
+            log.err("Failed to create frame", .{});
+            return error.FailedToCreateFrame;
+        }
+        defer rav1e.rav1e_frame_unref(frame);
+
+        const w = self.width;
+        const h = self.height;
+        rav1e.rav1e_frame_fill_plane(frame, 0, frame_data.ptr, w * h, w, 1);
+        rav1e.rav1e_frame_fill_plane(frame, 1, frame_data.ptr + w * h, w * h / 4, w / 2, 1);
+        rav1e.rav1e_frame_fill_plane(frame, 2, frame_data.ptr + w * h * 5 / 4, w * h / 4, w / 2, 1);
+
+        const status = rav1e.rav1e_send_frame(self.encoder, frame);
+        if (status != rav1e.RA_ENCODER_STATUS_SUCCESS) {
+            log.err("Failed to send frame: {}", .{status});
+            return error.FailedToSendFrame;
+        }
+
+        while (true) {
+            var packet: ?*rav1e.RaPacket = undefined;
+            const sts = rav1e.rav1e_receive_packet(self.encoder, &packet);
+
+            if (sts == rav1e.RA_ENCODER_STATUS_SUCCESS) {
+                cb(packet.?.data[0..packet.?.len]);
+                rav1e.rav1e_packet_unref(packet);
+            } else if (sts == rav1e.RA_ENCODER_STATUS_ENCODED) {
+                log.info("Encoded", .{});
+                continue;
+            } else if (sts == rav1e.RA_ENCODER_STATUS_NEED_MORE_DATA) {
+                log.info("Need more data", .{});
+                break;
+            } else {
+                log.err("Failed to receive packet: {}", .{sts});
+                return error.FailedToReceivePacket;
+            }
+        }
+    }
+    
+    pub fn flush(self: *Self, cb: *const fn([]const u8)void) !void {
+        const flush_status = rav1e.rav1e_send_frame(self.encoder, null);
+        if (flush_status != rav1e.RA_ENCODER_STATUS_SUCCESS) {
+            log.err("Failed to send null frame: {}", .{flush_status});
+            return error.FailedToSendNullFrame;
+        }
+
+        while (true) {
+            var packet: ?*rav1e.RaPacket = undefined;
+            const status = rav1e.rav1e_receive_packet(self.encoder, &packet);
+
+            if (status == rav1e.RA_ENCODER_STATUS_SUCCESS) {
+                cb(packet.?.data[0..packet.?.len]);
+                rav1e.rav1e_packet_unref(packet);
+            } else if (status == rav1e.RA_ENCODER_STATUS_ENCODED) {
+                log.info("Encoded", .{});
+                continue;
+            } else if (status == rav1e.RA_ENCODER_STATUS_LIMIT_REACHED) {
+                log.info("Limit reached", .{});
+                break;
+            } else {
+                log.err("Failed to receive packet: {}", .{status});
+                return error.FailedToReceivePacket;
+            }
+        }
+    }    
+};
---
+++ src/av1enc_test.zig
@@ -0,0 +1,100 @@
+const std = @import("std");
+const testing = std.testing;
+const fs = std.fs;
+const io = std.io;
+const mem = std.mem;
+const os = std.os;
+
+const IVF = @import("ivf.zig");
+const AV1Enc = @import("av1enc.zig").AV1Enc;
+
+var frame_count: u32 = 0;
+var ivf_writer: IVF.IVFWriter = undefined;
+
+test "encode test" {
+    try encode("testfiles/sample01.i420", "testfiles/output.ivf");
+    try checkIVF("testfiles/output.ivf");
+}
+
+fn encode(input_file: []const u8, output_file: []const u8) !void {
+    const alc = std.heap.page_allocator;
+
+    var yuv_file = try std.fs.cwd().openFile(input_file, .{});
+    defer yuv_file.close();
+
+    var outfile = try std.fs.cwd().createFile(output_file, .{});
+    defer outfile.close();
+
+    const width: u32 = 160;
+    const height: u32 = 120;
+    const bitrate: u32 = 1000;
+    const framerate: u32 = 15;
+    const time_scale: u32 = 1;
+    const keyframe_interval: u32 = 60;
+
+    const ivf_header = IVF.IVFHeader{
+        .signature = .{ 'D', 'K', 'I', 'F' },
+        .version = 0,
+        .header_size = 32,
+        .fourcc = .{ 'A', 'V', '0', '1' }, //"AV01",
+        .width = width,
+        .height = height,
+        .frame_rate = framerate,
+        .time_scale = time_scale,
+        .num_frames = 0,
+        .unused = 0,
+    };
+    ivf_writer = try IVF.IVFWriter.init(outfile, &ivf_header);
+    defer ivf_writer.deinit();
+
+    const yuv_size = width * height * 3 / 2;
+    var yuv_buf = try alc.alloc(u8, yuv_size);
+    defer alc.free(yuv_buf);
+
+    var av1enc = try AV1Enc.init(width, height, framerate, time_scale, bitrate, keyframe_interval);
+    defer av1enc.deinit();
+
+    while (true) {
+        if (yuv_size != try yuv_file.readAll(yuv_buf)) {
+            break;
+        }
+        try av1enc.sendFrame(yuv_buf, &callback);
+    }
+    try av1enc.flush(&callback);
+}
+
+fn callback(encoded_data: []const u8) void {
+    ivf_writer.writeIVFFrame(encoded_data, frame_count) catch |err| {
+        std.debug.print("callback: {s}\n", .{@errorName(err)});
+    };
+    frame_count += 1;
+}
+
+fn checkIVF(filename: []const u8) !void {
+    var file = try fs.cwd().openFile(filename, .{});
+    defer file.close();
+    var reader = try IVF.IVFReader.init(file);
+    defer reader.deinit();
+
+    try testing.expectEqualSlices(u8, &reader.header.fourcc, "AV01");
+    try testing.expect(reader.header.width == 160);
+    try testing.expect(reader.header.height == 120);
+    try testing.expect(reader.header.frame_rate == 15);
+    try testing.expect(reader.header.time_scale == 1);
+    try testing.expect(reader.header.num_frames == 75);
+
+    var frame_index: usize = 0;
+    while (true) {
+        var ivf_frame_header: IVF.IVFFrameHeader = undefined;
+        reader.readIVFFrameHeader(&ivf_frame_header) catch |err| {
+            if (err == error.EndOfStream) break;
+            return err;
+        };
+        try testing.expect(ivf_frame_header.timestamp == frame_index);
+
+        // Skip the frame data according to frame_size
+        try reader.skipFrame(ivf_frame_header.frame_size);
+
+        frame_index += 1;
+    }
+}
